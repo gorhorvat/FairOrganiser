d3cd9f2102fdcf11d5834e045dbdef1df65d7d3b
marhren <marhren2@gmail.com>
2016-05-02 00:00:22 +0200

    test za gorana

---
+++ FairOrganiser/MarkoHren/Sudionik.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkoHren
+{
+    class Sudionik
+    {
+        private string ime;
+        private string prezime;   
+    }
+}
