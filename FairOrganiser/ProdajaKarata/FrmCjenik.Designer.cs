431054dce1b95186543f6f748812493e1f448c78
gorhorvat <gorhorvat@foi.hr>
2016-06-09 23:13:01 +0200

    Setup-an EF model za ProdajaKarata te omogućeno spremanje novog cjenika u bazu.

--- FairOrganiser/ProdajaKarata/FrmCjenik.Designer.cs
+++ FairOrganiser/ProdajaKarata/FrmCjenik.Designer.cs
@@ -28,14 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnPregledaj = new System.Windows.Forms.Button();
             this.dgvPopisCjenika = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vrijediOdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vrijediDoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kartasDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipKartesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cjenikDogadajaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.btnObrisiCjenik = new System.Windows.Forms.Button();
             this.btnDodajCjenik = new System.Windows.Forms.Button();
             this.dtpDo = new System.Windows.Forms.DateTimePicker();
             this.dtpOd = new System.Windows.Forms.DateTimePicker();
             this.lblCjenikKarata = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPopisCjenika)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cjenikDogadajaBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPregledaj
@@ -46,17 +54,68 @@
             this.btnPregledaj.TabIndex = 18;
             this.btnPregledaj.Text = "Pregledaj cjenik";
             this.btnPregledaj.UseVisualStyleBackColor = true;
+            this.btnPregledaj.Click += new System.EventHandler(this.btnPregledaj_Click);
             // 
             // dgvPopisCjenika
             // 
+            this.dgvPopisCjenika.AutoGenerateColumns = false;
+            this.dgvPopisCjenika.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvPopisCjenika.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPopisCjenika.ColumnHeadersVisible = false;
+            this.dgvPopisCjenika.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.vrijediOdDataGridViewTextBoxColumn,
+            this.vrijediDoDataGridViewTextBoxColumn,
+            this.kartasDataGridViewTextBoxColumn,
+            this.tipKartesDataGridViewTextBoxColumn});
+            this.dgvPopisCjenika.DataSource = this.cjenikDogadajaBindingSource;
             this.dgvPopisCjenika.Location = new System.Drawing.Point(12, 54);
             this.dgvPopisCjenika.Name = "dgvPopisCjenika";
             this.dgvPopisCjenika.ReadOnly = true;
             this.dgvPopisCjenika.RowHeadersVisible = false;
-            this.dgvPopisCjenika.Size = new System.Drawing.Size(289, 190);
+            this.dgvPopisCjenika.Size = new System.Drawing.Size(503, 190);
             this.dgvPopisCjenika.TabIndex = 17;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // vrijediOdDataGridViewTextBoxColumn
+            // 
+            this.vrijediOdDataGridViewTextBoxColumn.DataPropertyName = "vrijediOd";
+            this.vrijediOdDataGridViewTextBoxColumn.HeaderText = "vrijediOd";
+            this.vrijediOdDataGridViewTextBoxColumn.Name = "vrijediOdDataGridViewTextBoxColumn";
+            this.vrijediOdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // vrijediDoDataGridViewTextBoxColumn
+            // 
+            this.vrijediDoDataGridViewTextBoxColumn.DataPropertyName = "vrijediDo";
+            this.vrijediDoDataGridViewTextBoxColumn.HeaderText = "vrijediDo";
+            this.vrijediDoDataGridViewTextBoxColumn.Name = "vrijediDoDataGridViewTextBoxColumn";
+            this.vrijediDoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // kartasDataGridViewTextBoxColumn
+            // 
+            this.kartasDataGridViewTextBoxColumn.DataPropertyName = "Kartas";
+            this.kartasDataGridViewTextBoxColumn.HeaderText = "Kartas";
+            this.kartasDataGridViewTextBoxColumn.Name = "kartasDataGridViewTextBoxColumn";
+            this.kartasDataGridViewTextBoxColumn.ReadOnly = true;
+            this.kartasDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // tipKartesDataGridViewTextBoxColumn
+            // 
+            this.tipKartesDataGridViewTextBoxColumn.DataPropertyName = "TipKartes";
+            this.tipKartesDataGridViewTextBoxColumn.HeaderText = "TipKartes";
+            this.tipKartesDataGridViewTextBoxColumn.Name = "tipKartesDataGridViewTextBoxColumn";
+            this.tipKartesDataGridViewTextBoxColumn.ReadOnly = true;
+            this.tipKartesDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // cjenikDogadajaBindingSource
+            // 
+            this.cjenikDogadajaBindingSource.DataSource = typeof(ProdajaKarata.CjenikDogadaja);
             // 
             // btnObrisiCjenik
             // 
@@ -66,6 +125,7 @@
             this.btnObrisiCjenik.TabIndex = 16;
             this.btnObrisiCjenik.Text = "Obriši cjenik";
             this.btnObrisiCjenik.UseVisualStyleBackColor = true;
+            this.btnObrisiCjenik.Click += new System.EventHandler(this.btnObrisiCjenik_Click);
             // 
             // btnDodajCjenik
             // 
@@ -75,11 +135,12 @@
             this.btnDodajCjenik.TabIndex = 15;
             this.btnDodajCjenik.Text = "Dodaj cjenik";
             this.btnDodajCjenik.UseVisualStyleBackColor = true;
+            this.btnDodajCjenik.Click += new System.EventHandler(this.btnDodajCjenik_Click);
             // 
             // dtpDo
             // 
             this.dtpDo.CustomFormat = "";
-            this.dtpDo.Location = new System.Drawing.Point(159, 14);
+            this.dtpDo.Location = new System.Drawing.Point(373, 12);
             this.dtpDo.Name = "dtpDo";
             this.dtpDo.Size = new System.Drawing.Size(142, 20);
             this.dtpDo.TabIndex = 14;
@@ -87,7 +148,7 @@
             // dtpOd
             // 
             this.dtpOd.CustomFormat = "";
-            this.dtpOd.Location = new System.Drawing.Point(11, 14);
+            this.dtpOd.Location = new System.Drawing.Point(203, 12);
             this.dtpOd.Name = "dtpOd";
             this.dtpOd.Size = new System.Drawing.Size(142, 20);
             this.dtpOd.TabIndex = 13;
@@ -105,7 +166,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 293);
+            this.ClientSize = new System.Drawing.Size(558, 293);
             this.Controls.Add(this.btnPregledaj);
             this.Controls.Add(this.dgvPopisCjenika);
             this.Controls.Add(this.btnObrisiCjenik);
@@ -115,7 +176,9 @@
             this.Controls.Add(this.lblCjenikKarata);
             this.Name = "FrmCjenik";
             this.Text = "Cjenici";
+            this.Load += new System.EventHandler(this.FrmCjenik_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPopisCjenika)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cjenikDogadajaBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +193,12 @@
         private System.Windows.Forms.DateTimePicker dtpDo;
         private System.Windows.Forms.DateTimePicker dtpOd;
         private System.Windows.Forms.Label lblCjenikKarata;
+        private System.Windows.Forms.BindingSource cjenikDogadajaBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vrijediOdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vrijediDoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kartasDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipKartesDataGridViewTextBoxColumn;
     }
 }
 
--- FairOrganiser/ProdajaKarata/FrmCjenik.cs
+++ FairOrganiser/ProdajaKarata/FrmCjenik.cs
@@ -15,5 +15,61 @@ namespace ProdajaKarata
         {
             InitializeComponent();
         }
+
+        private void FrmCjenik_Load(object sender, EventArgs e)
+        {
+            PrikaziCjenike();
+        }
+        /// <summary>
+        /// Dohvaća listu svih cjenika događaja u kontekstu, te ih prikazuje u DataGridView-u.
+        /// </summary>
+        private void PrikaziCjenike()
+        {
+            BindingList<CjenikDogadaja> popisCjenika = null;
+            using (var db = new ProdajaKarataEntities())
+            {
+                popisCjenika = new BindingList<CjenikDogadaja>(db.CjenikDogadajas.ToList());
+            }
+            cjenikDogadajaBindingSource.DataSource = popisCjenika;
+        }
+        /// <summary>
+        /// Extract-a datume iz DateTimePickera i pristiskom na tipku Dodaj sprema novi cjenik u bazu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDodajCjenik_Click(object sender, EventArgs e)
+        {
+            using (var db = new ProdajaKarataEntities())
+            {
+                CjenikDogadaja noviCjenik = new CjenikDogadaja
+                {
+                    vrijediOd = dtpOd.Value.Date,
+                    vrijediDo = dtpDo.Value.Date
+                };
+                db.CjenikDogadajas.Add(noviCjenik);
+                db.SaveChanges();
+            }
+
+            PrikaziCjenike();
+        }
+        /// <summary>
+        /// Briše selektirani cjenik iz DataGridView-a te iz baze.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnObrisiCjenik_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Omogućuje pregled odabranog cjenika u novoj formi.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPregledaj_Click(object sender, EventArgs e)
+        {
+            FrmPregledCjenika pregled = new FrmPregledCjenika();
+            pregled.Show();
+        }
     }
 }
