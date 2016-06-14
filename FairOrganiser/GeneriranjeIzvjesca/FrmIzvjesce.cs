38c76b9d06f795b1dcc11fc84e69a04511644d4a
gorhorvat <gorhorvat@foi.hr>
2016-06-14 02:35:28 +0200

    Međusobno povezani EF-ovi iz svih projekata sa EF-om u glavnom projektu. Započeto GeneriranjeIzvjesca.

--- FairOrganiser/GeneriranjeIzvjesca/FrmIzvjesce.cs
+++ FairOrganiser/GeneriranjeIzvjesca/FrmIzvjesce.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GeneriranjeIzvjesca
@@ -14,6 +15,17 @@ namespace GeneriranjeIzvjesca
         public FrmIzvjesce()
         {
             InitializeComponent();
+        }
+
+        private void FrmIzvjesce_Load(object sender, EventArgs e)
+        {
+            List<string> popisIzvjesca = new List<string>() { "Izvješće o broju posjetitelja", "Izvješće o prodanim uslugama", "Izvješće o popunjenosti kapaciteta", "Izvješće o organiziranim događajima" };
+            cmbNazivIzvjesca.DataSource = popisIzvjesca;
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
